6b1bc0a2551e0957bc6af330ac2acc2cd0e6bd1c
areej-nasser <areejnasser456@gmail.com>
2026-01-03 12:13:11 +0200

    first commit

---
+++ background.js
@@ -0,0 +1,18 @@
+// Create context menu
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.contextMenus.create({
+        id: 'factCheck',
+        title: 'Fact Check "%s"',
+        contexts: ['selection']
+    });
+});
+
+// Handle context menu clicks
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === 'factCheck') {
+        // Open popup or trigger fact check
+        chrome.action.openPopup();
+
+
+    }
+});
